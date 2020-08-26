c725bc5b1a98a980258ac182f53ed39a5bd1b3d4
d0561910 <D0561910@mail.fcu.edu.tw>
2020-08-26 16:41:52 +0800

    setting login UI

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,42 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home";
+import Login from "../views/Login";
+import Register from "../views/Register";
 
-Vue.use(VueRouter)
-
-  const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+Vue.use(VueRouter);
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
+  routes: [
+    {
+      path: "/home",
+      name: "Home",
+      component: Home,
+    },
+    {
+      path: "/about",
+      name: "About",
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () =>
+        import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    },
+    {
+      path: "/login",
+      name: "Login",
+      component: Login,
+    },
+    {
+      path: "/register",
+      name: "Create New Account",
+      component: Register,
+    },
 
-export default router
+    { path: "*", redirect: "/login" },
+  ],
+});
+
+export default router;
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,15 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+import { alert } from "./alert.module";
+import { account } from "./account.module";
+import { users } from "./users.module";
+
+Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+  state: {},
+  mutations: {},
+  actions: {},
+  modules: { alert, account, users },
+});
